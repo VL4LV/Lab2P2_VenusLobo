da83b56033a3a96357d70b19a5177156bfd440df
VL4LV <venus.lobo@unitec.edu>
2024-01-26 13:54:06 -0600

    Primer comit, creada la clase de usuario

---
+++ Lab2P2_VenusLobo/src/lab2p2_venuslobo/Lab2P2_VenusLobo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab2p2_venuslobo;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Lab2P2_VenusLobo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
